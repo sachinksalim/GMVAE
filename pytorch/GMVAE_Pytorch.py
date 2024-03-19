54d8c6f203b4e553475666c5b519fb16e4aac9ca
Sachin Salim <sachinksalim@gmail.com>
2024-03-19 09:11:55 -0400

    updated main driver code

--- pytorch/GMVAE_Pytorch.py
+++ pytorch/GMVAE_Pytorch.py
@@ -1,23 +1,12 @@
-"""
----------------------------------------------------------------------
--- Author: Jhosimar George Arias Figueroa
----------------------------------------------------------------------
-
-Main file to execute the model on the MNIST dataset
-
-"""
-import matplotlib
-matplotlib.use('agg')
-import matplotlib.pyplot as plt
+import numpy as np
 import argparse
 import random
-import numpy as np
-import os
-import torch
 from torchvision import datasets, transforms
 from torch.utils.data.sampler import SubsetRandomSampler
 import torch.utils.data
+from scipy.io import loadmat
 from model.GMVAE import *
+import matplotlib.pyplot as plt
 
 #########################################################
 ## Input Parameters
@@ -29,9 +18,9 @@ parser.add_argument('-f', '--file',
                     help='Path for input file. First line should contain number of lines to search in')
 
 ## Dataset
-parser.add_argument('--dataset', type=str, choices=['mnist', 'cifar100'],
+parser.add_argument('--dataset', type=str, choices=['mnist'],
                     default='mnist', help='dataset (default: mnist)')
-parser.add_argument('--seed', type=int, default=0, help='random seed (default: 0)')
+parser.add_argument('--seed', type=int, default=1, help='random seed (default: 1)')
 
 ## GPU
 parser.add_argument('--cuda', type=int, default=1,
@@ -48,7 +37,7 @@ parser.add_argument('--batch_size_val', default=200, type=int,
                     help='mini-batch size of validation (default: 200)')
 parser.add_argument('--learning_rate', default=1e-3, type=float,
                     help='learning rate (default: 0.001)')
-parser.add_argument('--decay_epoch', default=-1, type=int, 
+parser.add_argument('--decay_epoch', default=-1, type=int,
                     help='Reduces the learning rate every decay_epoch')
 parser.add_argument('--lr_decay', default=0.5, type=float,
                     help='Learning rate decay for training (default: 0.5)')
@@ -58,7 +47,7 @@ parser.add_argument('--num_classes', type=int, default=10,
                     help='number of classes (default: 10)')
 parser.add_argument('--gaussian_size', default=64, type=int,
                     help='gaussian size (default: 64)')
-parser.add_argument('--input_size', default=784, type=int,
+parser.add_argument('--input_size', default=3072, type=int,
                     help='input size (default: 784)')
 
 ## Partition parameters
@@ -68,17 +57,17 @@ parser.add_argument('--train_proportion', default=1.0, type=float,
 ## Gumbel parameters
 parser.add_argument('--init_temp', default=1.0, type=float,
                     help='Initial temperature used in gumbel-softmax (recommended 0.5-1.0, default:1.0)')
-parser.add_argument('--decay_temp', default=1, type=int, 
+parser.add_argument('--decay_temp', default=1, type=int,
                     help='Set 1 to decay gumbel temperature at every epoch (default: 1)')
-parser.add_argument('--hard_gumbel', default=0, type=int, 
+parser.add_argument('--hard_gumbel', default=0, type=int,
                     help='Set 1 to use the hard version of gumbel-softmax (default: 1)')
-parser.add_argument('--min_temp', default=0.5, type=float, 
+parser.add_argument('--min_temp', default=0.5, type=float,
                     help='Minimum temperature of gumbel-softmax after annealing (default: 0.5)' )
 parser.add_argument('--decay_temp_rate', default=0.013862944, type=float,
                     help='Temperature decay rate at every epoch (default: 0.013862944)')
 
 ## Loss function parameters
-parser.add_argument('--w_gauss', default=1, type=float,
+parser.add_argument('--w_gauss', default=2, type=float,
                     help='weight of gaussian loss (default: 1)')
 parser.add_argument('--w_categ', default=1, type=float,
                     help='weight of categorical loss (default: 1)')
@@ -90,18 +79,12 @@ parser.add_argument('--rec_type', type=str, choices=['bce', 'mse'],
 ## Others
 parser.add_argument('--verbose', default=0, type=int,
                     help='print extra information at every epoch.(default: 0)')
-parser.add_argument('--random_search_it', type=int, default=20,
-                    help='iterations of random search (default: 20)')
-
 parser.add_argument('--save_file', required=True,
-              help='Path for file to save model and history')
-parser.add_argument('--desc', default='VAE model', type=str,
-              help='Description of the model')
+                    help='Path for file to save model and history')
+parser.add_argument('--desc', required=True,
+                    help='Description of the model')
 
 args = parser.parse_args()
-
-if args.cuda == 1:
-   os.environ["CUDA_VISIBLE_DEVICES"] = str(args.gpuID)
 
 ## Random Seed
 SEED = args.seed
@@ -111,29 +94,20 @@ torch.manual_seed(SEED)
 if args.cuda:
   torch.cuda.manual_seed(SEED)
 
-#########################################################
-## Read Data
-#########################################################
-if args.dataset == "mnist":
-  print("Loading mnist dataset...")
-  # Download or load downloaded MNIST dataset
-  train_dataset = datasets.MNIST('./data/mnist', train=True, download=True, transform=transforms.ToTensor())
-  test_dataset = datasets.MNIST('./data/mnist', train=False, transform=transforms.ToTensor())
-elif args.dataset == "cifar100":
-  print("Loading cifar100 dataset...")
-  transform = transforms.Compose([
+print("Loading mnist dataset...")
+
+transform = transforms.Compose([
     transforms.ToTensor(),
-    # transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
-  ])
-  # Download CIFAR-100 dataset
-  train_dataset = datasets.CIFAR100(root='./data/cifar100', train=True, download=True, transform=transform)
-  test_dataset = datasets.CIFAR100(root='./data/cifar100', train=False, download=True, transform=transform)
+    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+])
 
+# Download CIFAR-100 dataset
+train_dataset = datasets.CIFAR100(root='./data', train=True, download=True, transform=transform)
+test_dataset = datasets.CIFAR100(root='./data', train=False, download=True, transform=transform)
 
+# print dataset shape
+print('Train size: ', len(train_dataset), ' Test size: ', len(test_dataset))
 
-#########################################################
-## Data Partition
-#########################################################
 def partition_dataset(n, proportion=0.8):
   train_num = int(n * proportion)
   indices = np.random.permutation(n)
@@ -141,35 +115,27 @@ def partition_dataset(n, proportion=0.8):
   return train_indices, val_indices
 
 if args.train_proportion == 1.0:
+  # we use all train dataset without partitioning
   train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=args.batch_size, shuffle=True)
   test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=args.batch_size_val, shuffle=False)
   val_loader = test_loader
 else:
+  # partition dataset according to train_proportion
   train_indices, val_indices = partition_dataset(len(train_dataset), args.train_proportion)
-  # Create data loaders for train, validation and test datasets
+  # create data loaders for train, validation and test datasets
   train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=args.batch_size, sampler=SubsetRandomSampler(train_indices))
   val_loader = torch.utils.data.DataLoader(train_dataset, batch_size=args.batch_size_val, sampler=SubsetRandomSampler(val_indices))
   test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=args.batch_size_val, shuffle=False)
 
-## Calculate flatten size of each input data
-args.input_size = np.prod(train_dataset[0][0].size())
-print(args.input_size)
-#########################################################
-## Train and Test Model
-#########################################################
+
+# Model Initialization
 gmvae = GMVAE(args)
 
-## Training Phase
+# Training Phase
 history_loss = gmvae.train(train_loader, val_loader)
-
-## Testing Phase
-accuracy, nmi = gmvae.test(test_loader)
-print("Testing phase...")
-print("Accuracy: %.5lf, NMI: %.5lf" % (accuracy, nmi) )
 
 torch.save({
     'config': args,
     'model_state': gmvae.network.state_dict(),
     'loss_history': history_loss,
-}, f"checkpoint/{args.save_file}.pth")
-
+}, f"data/{args.save_file}.pth")
